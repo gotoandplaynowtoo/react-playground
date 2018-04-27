3c0497d341b9291e2925c384c3a55897cbb0597c
Donde <gotoandplaynowtoo@gmail.com>
2018-04-27 16:02:56 +0800

    fixing env

--- lesson-002/src/App.js
+++ lesson-002/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import SearchBar from 'components/SearchBar';
 
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 class App extends Component {
   render() {
     return (
--- lesson-002/src/components/SearchBar.js
+++ lesson-002/src/components/SearchBar.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
+class Test extends Component {
+  constructor(props) {
+    super(props);
+    console.log('instantiated');
+  }
+
+  render = () => {
+    console.log('i am rerendered too');
+    return <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum enim illo excepturi exercitationem rem eligendi dolores, qui ab porro facere quae error ea adipisci vitae blanditiis ipsum sit voluptatibus. Aliquam.</p>
+  };
+}
+
 class SearchBar extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+  }
+
+  onInputChange = (e) => {
+    this.setState({
+      term: e.target.value
+    });
+  };
+
+  render = () => {
     return (
-      <input />
+      <div>
+        <input 
+          value={this.state.term}
+          onChange={this.onInputChange} 
+        />
+        <Test/>
+      </div>
     );
   }
 }
